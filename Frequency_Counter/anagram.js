5aa874e54cf01a4696917fa3a3960f1caf80f443
Hema Lohana <hemalohana8@gmail.com>
2023-08-21 21:25:50 +0530

    files updated

---
+++ Frequency_Counter/anagram.js
@@ -0,0 +1,70 @@
+//Method-1 : valid anagram
+
+function isAnagram(s,t){
+    if(s.length!= t.length)
+    return false;
+    let map1 = new Map();
+    let map2 = new Map();
+    for(let key of s){
+        if(map1.has(key)){
+            let oldFrq = map1.get(key);
+            let newFrq = oldFrq+ 1 ;
+            map1.set(key, newFrq);
+        }
+        else{
+            map1.set(key,1);
+        }
+    }
+    for(let key of t){
+        if(map2.has(key)){
+            let oldFrq = map2.get(key);
+            let newFrq = oldFrq + 1;
+            map2.set(key, newFrq);
+        }
+        else{
+            map2.set(key, 1);
+        }
+       
+    }
+    for(let [key1, value1] of map1){
+        let value2 = map2.get(key1);
+        if(value1 != value2)
+        return false;
+    }
+    return true;
+}
+console.log(isAnagram("abcdbc","abbccd"));
+
+
+//Method-2 : valid anagram
+
+// function isAnagram(s,t){
+//     let map = new Map();
+//     for(let char of s){
+//         if(map.has(char)){
+//             let oldFrq = map.get(char);
+//             let newFrq = oldFrq +1;
+//             map.set(char, newFrq)
+//         }
+//         else{
+//             map.set(char, 1);
+//         }
+//     }
+//     for(let char of t){
+//         if(map.has(char)){
+//             let oldFrq = map.get(char);
+//             let newFrq = oldFrq - 1;
+//             map.set(char, newFrq);
+//         }
+//         else{
+//             return false;
+//         }
+//     }
+//     for(let [key, value] of map){
+//         if(value != 0){
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+// console.log(isAnagram("abcdac","aabccd"));
---
+++ twoPointer/twoSum.js
@@ -0,0 +1,45 @@
+//Method:-1
+// function twoSum(arr, x){
+//      for(let i=0;i<arr.length;i++){
+//         for(let j=i+1;j<arr.length;j++){
+//             if(arr[i]+arr[j]==x) return [i,j]
+//         }
+//      }
+//      return [-1,-1]
+// }
+// // console.log(twoSum([1,2,3,4,5,6,7,8,9], 11));
+// console.log(twoSum([1,6,9,12,17,23,50],35));
+//timeComplexity: O(n^2), spaceComplexity:O(1)
+
+
+//Method:-2
+// function twoSum(arr,x){
+//     let map = new Map();
+//     for(let i=0;i<arr.length;i++){
+//         if(map.has(x-arr[i])){
+//            let j=map.get(x-arr[i]);
+//            return [j,i];
+//         }
+//         else{
+//             map.set(arr[i],i);
+//         }
+//     }
+// }
+// console.log(twoSum([1,2,3,4,5,6,7,8,9], 11));
+//timeCOmplexity:- O(n) spaceComplexity:-O(1)
+
+//method:-3
+// function twoSum(arr,x){
+//     let i=0,j=arr.length-1;
+//     while(i<j){
+//        let sum = arr[i]+arr[j];
+//        if(sum==x) return [i,j];
+//        else if(sum<x) i++;
+//        else j--;
+//     }
+//     return [-1,-1]
+// }
+// console.log(twoSum([1,2,3,4,5,6,7,8,9], 11));
+//timeCOmplexity:- O(1) spaceComplexity:-O(n)
+
+
