ccd861d817b48ced8be86ad6ca4a30f5c8e7d3af
Hema Lohana <hemalohana8@gmail.com>
2024-01-29 20:55:56 +0530

    some questions added with their solution in tree

--- Tree/queOfTree.js
+++ Tree/queOfTree.js
@@ -1,15 +1,123 @@
+// Binary Tree Preorder Traversal
+// var preorderTraversal = function(root) {
+//     let arr = [];
+//     function traverse(root){
+//         if(root == null) return;
+//         arr.push(root.val);
+//         traverse(root.left);
+//         traverse(root.right);
+//     }
+//     traverse(root);
+//     return arr;
+//  };
 
-// 104. Maximum Depth of Binary Tree
 
-// Given the root of a binary tree, return its maximum depth.
+// Binary Tree Postorder Traversal
+// var postorderTraversal = function(root) {
+//     let arr = [];
+//     function traverse(root){
+//     if(root == null) return;
+//     traverse(root.left);
+//     traverse(root.right);
+//     arr.push(root.val)
+//     }
+//     traverse(root);
+//     return arr;
+// };
 
-// A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+// Binary Tree Inorder Traversal
+// var inorderTraversal = function(root) {
+//     let arr = [];
+//     function traverse(root){
+//         if(root == null) return;
+//         traverse(root.left);
+//         arr.push(root.val);
+//         traverse(root.right);
+//     }
+//     traverse(root);
+//     return arr;
+// };
 
-// Input: root = [3,9,20,null,null,15,7]
-// Output: 3
-var maxDepth = function(root) {
-    if(root == null) return 0;
-    let l = maxDepth(root.left);
-    let r = maxDepth(root.right);
-    return Math.max(l,r)+1;
-};
+// Maximum Depth of Binary Tree/height of tree
+// var maxDepth = function(root) {
+//     if(root == null) return 0;
+//     let l = maxDepth(root.left);
+//     let r = maxDepth(root.right);
+//     return Math.max(l,r)+1;
+// };
+
+
+// Same Tree
+// var isSameTree = function(p, q) {
+//     if(p==null && q==null) return true;
+//     if(p==null || q==null) return false;
+//     if(p.val != q.val) return false;
+//     let l = isSameTree(p.left,q.left);
+//     if(l==false) return false;
+//     let r = isSameTree(p.right,q.right);
+//     if(r==false) return false;
+//     return true;
+//  };
+
+// Symmetric Tree
+// var isSymmetric = function(root) {
+//     function mirrorEffect(p,q){
+//         if(p==null && q==null) return true;
+//         if(p==null || q==null) return false;
+//         if(p.val!=q.val) return false;
+//         let l = mirrorEffect(p.left,q.right);
+//         let r = mirrorEffect(p.right,q.left);
+//         return l && r;
+//     }
+//     return mirrorEffect(root,root);
+// };
+
+
+// Balanced Binary Tree/height ki diff 1 ya 0 honi chahiye usse jyada ho to false
+// var isBalanced = function(root) {
+//     function balance(root){
+//       if(root==null) return [0,true];
+//       let [height1,balanceLeft] = balance(root.left);
+//       let [height2,balanceRight] = balance(root.right);
+//       if(!balanceLeft) return [height1,false];
+//       if(!balanceRight) return [height2,false];
+//       if(Math.abs(height1-height2)>1) return [height1,false];
+//       return [Math.max(height1,height2)+1,true] 
+//     }
+//    let [h,res] = balance(root);
+//    return res;
+// };
+
+//max tree
+// function maxTree(root){
+//     if(root == null) return -Infinity;
+//     let l = maxTree(root.left);
+//     let r = maxTree(root.right);
+//     return Math.max(l,root.val,r);
+// }
+
+
+//min tree
+// function minTree(root){
+//     if(root == null) return Infinity;
+//     let l = minTree(root.left);
+//     let r=minTree(root.right);
+//     return Math.min(l,root,r);
+// }
+
+
+//height of tree
+// function heightOfTree(root){
+//     if(root==null) return 0;
+//     let l=heightOfTree(root.left);
+//     let r= heightOfTree(root.right);
+//     return Math.max(l,r)+1;
+// }
+
+//sum of tree
+function sumOfTree(root){
+    if(root==null) return 0;
+    let l=sumOfTree(root.left);
+    let r=sumOfTree(root.right);
+    return l+r+root.val;
+}
