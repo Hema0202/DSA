3655eff723c20f0e4bdb1efe5e332f6ce6368bd7
Hema0202 <hemalohana8@gmail.com>
2024-03-01 05:06:32 +0530

    added new programme of sliding window

--- sliding_window.js
+++ sliding_window.js
@@ -78,43 +78,138 @@ function subArraySumOfSize(n,arr,k){
 // Ex1: n = 8, arr = [3, 5, -1, 3, -1, 4, 5, 6], k = 3;
 // op1: [2, 2, 2, 4, 4, -1]
 
-function negInSubarray(n, arr, k){
-    let res = [];
-    let last = -1;
-    for(let i=0;i<=n-k;i++) {
-        for(let j=i;j<i+k;j++){
-            if(arr[j] < 0){
-                res.push(j);
-                break;
-            }
-        }
-    }
-    console.log(res);
-}
-arr = [3, 5, -1, 3, -1, 4, 5, 6], n= 8, k = 3;
-negInSubarray(n, arr, k);
+// function negInSubarray(n, arr, k){
+//     let res = [];
+//     for(let i=0;i<=n-k;i++){
+//         for(let j=i;j<=i+k;j++){
+//             if(arr[j]<=0){
+//                 res.push(j);
+//                 break;
+//             }
+//         }
+//     }
+//     console.log(res);
+// }
 
-function negInSubarray(n, arr, k) {
-    let res = [];
-    let last = -1;
-    for(let i=0;i<n-k;i++) {
-        if(last >= i) res.push(last);
-        else {
+//////////////////////////optimized code with sliding window
+
+function negInSubarray(n, arr, k){
+    let res=[];
+    let last=-1;
+    for(let i=0;i<=n-k;i++){
+        if(i<= last) res.push(last);
+        else{
             let isFound = false;
-            for(let j=i;j<i+k;j++) {
-                if(arr[j] < 0) {
+            for(let j=i;j<i+k;j++){
+                if(arr[j]<0){
                     res.push(j);
                     last = j;
                     isFound = true;
                     break;
                 }
             }
-
-            if(!isFound) {
+            if(!isFound){
                 res.push(-1);
             }
         }
     }
-
     console.log(res);
 }
+
+// let n = 8, arr = [3, 5, -1, 3, -1, 4, 5, 6], k = 3;
+// negInSubarray(n,arr,k);
+
+
+// 4. Given an array of integers nums and a positive integer k,
+// find the largest subarrays with k distinct numbers.
+
+// Ex1: n = 11, arr = [3, 5, 6, 3, 3, 4, 5, 5, 5, 6, 7], k = 3;
+// op1: 6
+
+ 
+// function maxSizeSubArrWithDisNumber(n, arr, k){
+//     let max = -1;
+//     for(let i=0;i<n;i++) {
+//         let set = new Set();
+//         let count = 0;
+//         for(let j=i;j<n;j++){
+//             set.add(arr[j]);
+//             count++;
+//             if(set.size == k) max = Math.max(max, count);
+//             else if(set.size > k) break;
+//         }
+//     }
+//     console.log(max);
+// }
+
+// let n = 11, arr = [3, 5, 6, 3, 3, 4, 5, 5, 5, 6, 7], k = 3;
+
+// maxSizeSubArrWithDisNumber(n, arr, k);
+
+
+//////////////////////////optimized code with sliding window
+
+function maxSizeSubArrWithDisNumber(n, arr, k){
+    let max = -1, l=0, map = new Map();
+    for(let r=0;r<n;r++){
+        //Add element in map
+        if(map.has(arr[r])) map.set(arr[r], map.get(arr[r] + 1));
+        else map.set(arr[r], 1);
+
+        //check if distinct element exceeds k
+        if(map.size>k){
+            while(map.size != k){
+                map.set(arr[l], map.get(arr[l]) - 1);
+                if(map.get(arr[l]) == 0) map.delete(arr[l]);
+                l++;
+            }
+        }
+
+        //check is size of map equals k
+        if(map.size == k) max = Math.max(max, r-l+1);
+    }
+    console.log(max);
+}
+
+
+// let n = 11, arr = [3, 5, 6, 3, 3, 4, 5, 5, 5, 6, 7], k = 3;
+
+// maxSizeSubArrWithDisNumber(n, arr, k);
+
+
+// 5. Given a string s, find the length of the longest substring without repeating characters.
+// Example 1:
+// Input: s = "abcabcbb"
+// Output: 3
+// Explanation: The answer is "abc", with the length of 3.
+
+// function lengthOfLongestSubstring(s) {
+//     let max= 0;
+//     for(let i=0;i<s.length;i++){
+//         let set = new Set();
+//         for(let j=i;j<s.length;j++){
+//             if(set.has(s[j])) break;
+//             set.add(s[j]);
+//             max = Math.max(max, j-i+1);
+//         }
+//     }
+//     console.log(max);
+// }
+
+//////////////////////////optimized code with sliding window
+
+
+function lengthOfLongestSubstring(s){
+    let set = new Set(), l=0, max = 0;
+    for(let r=0;r<s.length;r++){
+        while(set.has(s[r])){
+            set.delete(s[l]);
+            l++;
+        }
+        set.add(s[r]);
+        max = Math.max(max, r-l+1);
+    }
+    console.log(max);
+}
+
+lengthOfLongestSubstring("abcabcbb");
