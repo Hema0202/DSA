859eaefc85adebf128bbaf89d66bf5cda0217788
Hema0202 <hemalohana8@gmail.com>
2024-03-26 22:22:56 +0530

    qod added

---
+++ LeetCode/qod/March/26_first-missing-positive.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var firstMissingPositive = function(nums) {
+    let num = 1;
+    nums.sort((a,b)=>a-b);
+
+    for(let i=0;i<nums.length;i++){
+        if(nums[i]===num){
+          num++;
+        }
+        else{
+            continue;
+        }
+    }
+    return num;
+};
