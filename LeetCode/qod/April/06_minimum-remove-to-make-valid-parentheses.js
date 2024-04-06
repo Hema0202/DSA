0c0bce808166c882bd4dd9b3716d3246ac718b8d
Hema0202 <hemalohana8@gmail.com>
2024-04-06 20:51:11 +0530

    qod added

---
+++ LeetCode/qod/April/06_minimum-remove-to-make-valid-parentheses.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var minRemoveToMakeValid = function (s) {
+    let arr = s.split('');
+    let stack = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === "(") {
+            stack.push(i);
+        }
+        else if (arr[i] === ")" && stack.length) {
+            stack.pop();
+        }
+        else if (arr[i] === ")") {
+            arr[i] = "";
+        }
+    }
+
+    while (stack.length) {
+        arr[stack.pop()] = "";
+    }
+
+    return arr.join("");
+};
