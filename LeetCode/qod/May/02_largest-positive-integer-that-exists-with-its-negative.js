be2794ce8827179e0dde6e3f4b275ebd4e1ec255
Hema0202 <hemalohana8@gmail.com>
2024-05-02 05:36:06 +0530

    qod added

---
+++ LeetCode/qod/May/02_largest-positive-integer-that-exists-with-its-negative.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMaxK = function(nums) {
+    nums.sort((a,b)=>b-a);
+    for(let i=0;i<nums.length;i++){
+        if(nums.includes(-nums[i])){
+            return nums[i];
+        }
+    }
+    return -1;
+};
